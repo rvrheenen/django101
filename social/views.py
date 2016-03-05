8b5717d3d17c082f23a3539674c09719f541d09b
rvrheenen <rick.van.rheenen@gmail.com>
2016-03-05 14:04:01 +0100

    delete posts

--- social/views.py
+++ social/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseBadRequest, HttpResponseForbidden
 from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from social.models import Comment, Post
@@ -25,8 +25,17 @@ def social_login(request):
 
 @login_required
 def home(request):
-    posts = Post.objects.all().order_by('date_time')
-    return render(request, 'social/home.html', {'posts': posts})
+    if request.method == 'GET':
+        posts = Post.objects.all()
+    elif request.method == 'POST':
+        check = _check_post_request(request, ['search_terms'])
+        if check[0]:
+            search_term = request.POST['search_terms']
+            posts = Post.objects.filter(text__icontains=search_term)
+        else:
+            return HttpResponseBadRequest(check[1])
+    posts = posts.order_by('-date_time')
+    return render(request, 'social/home.html', {'posts': posts, 'user': request.user})
 
 @login_required
 def add_post(request):
@@ -43,6 +52,15 @@ def add_post(request):
         return HttpResponseRedirect(reverse('social:home'))
     else:
         return HttpResponseBadRequest(check[1])
+
+@login_required
+def delete_post(request, post_id):
+    post = Post.objects.get(pk=post_id)
+    if request.user != post.poster:
+        return HttpResponseForbidden("You can only delete your own posts!")
+    else:
+        post.delete()
+        return HttpResponseRedirect(reverse('social:home'))
 
 @login_required
 def add_comment(request):
